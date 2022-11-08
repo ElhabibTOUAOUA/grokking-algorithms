f801425f49a849a332717562a65901186e4f87d5
elhabib_t <26313796+ElhabibTOUAOUA@users.noreply.github.com>
2022-11-08 21:47:27 +0100

    binary-search-algo

---
+++ binary-search/main.py
@@ -0,0 +1,18 @@
+
+def binary_search(arr, num):
+  low = 0
+  high = len(arr) - 1
+  while low <= high:
+    mid = int((high + low) / 2)
+    if(arr[mid] == num):
+      return mid
+    elif (arr[mid] > num):
+      high = mid - 1
+    else:
+      low = mid + 1
+  return None 
+
+numList = [2, 4, 7, 9, 15, 35, 44, 56]
+
+print(binary_search(numList, 13))
+print(binary_search(numList, 35))
